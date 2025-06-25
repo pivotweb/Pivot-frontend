a53a86ec715da8eaf766894f900f1bd4af7af35b
Okafor Ifeanyi <ifeanyi.okafor.bio@gmail.com>
2025-06-25 13:01:14 +0100

    feat(stories): completed stories and roleplay

---
+++ src/components/Pivot24/Pivot24.tsx
@@ -0,0 +1,23 @@
+export const Pivot24 = () => {
+    return (
+    <section className="my-10">
+        <div className="flex justify-between">
+            <h1 className="text-3xl font-bold mb-4 w-1/2">2024 Pivot Challenge Recap</h1>
+            <p className="text-xl text-gray-700 w-1/2">2024 was our first bold step. We brought MSMEs together, ran practical training, and saw real transformation. Want to know what worked and how it started? The 2024 recap will show snapshots, and key lessons from our pivot that paved the way for 2025.</p>
+        </div>
+        <div className="my-15 flex flex-col gap-5">
+            <div className="w-full h-[400px] overflow-hidden">
+                <img src="images/pivot1.jpg" alt="" className="w-full h-full object-cover rounded-2xl"/>
+            </div>
+            <div className="flex gap-5">
+                <div className="w-1/2 h-[400px] overflow-hidden">
+                    <img src="images/pivot2.jpg" alt="" className="w-full h-full object-cover rounded-2xl"/>
+                </div>
+                <div className="w-1/2 h-[400px] overflow-hidden">
+                    <img src="images/pivot3.jpg" alt="" className="w-full h-full object-cover rounded-2xl"/>
+                </div>
+                </div>
+        </div>
+    </section>
+    )
+}
